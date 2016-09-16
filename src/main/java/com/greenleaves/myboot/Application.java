65edea7bc09c72fdb3953bd26a9b72aacc9da77a
Nguyen Duy Tiep <duytiep@gmail.com>
2016-09-16 22:01:07 +0800

    first commit

---
+++ src/main/java/com/greenleaves/myboot/Application.java
@@ -0,0 +1,15 @@
+package com.greenleaves.myboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Nguyen Duy Tiep on 16-Sep-16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
